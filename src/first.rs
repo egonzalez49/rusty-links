6248eb57189eed541f7a31b0c1c4c381939a0d93
Elmer Gonzalez <gonzalezelmerf@gmail.com>
2023-09-17 19:12:58 -0400

    bad linked list implementation

---
+++ src/first.rs
@@ -0,0 +1,100 @@
+use std::mem;
+
+// Basically, by default anything with a size known at compile-time is by default stored on the stack.
+// The heap is generally for things which can grow, shrink, or otherwise have a size unknowable at compile-time.
+// Any given enum is always the same size in bytes, even if one of its members is a reference to a growable type.
+// The enum itself, including its reference field, will live on the stack, while the referent will live on the heap.
+
+// Allocates the initial variable on the stack, and each linked node on the heap (due to Box).
+// Allocates Empty variant when it's not needed and wastes space (enum is size of largest variant).
+// pub enum List {
+//     Empty,
+//     Elem(i32, Box<List>),
+// }
+
+pub struct List {
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+// Compiler implementation of drop for boxes would result in non-tail recursive calls which could blow up the stack.
+// We implement drop ourselves in a tail-recursive manner by hoisting nodes out of the boxes.
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty)
+            // boxed_node goes out of scope and gets dropped here;
+            // but its Node's `next` field has been set to Link::Empty
+            // so no unbounded recursion occurs.
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
